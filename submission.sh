c2b6b9f9b00793a5af48e1b612e5e887939819ce
Johnny Valles <vallesja@uci.edu>
2023-02-09 14:08:13 -0800

    create submission script

---
+++ submission.sh
@@ -0,0 +1,8 @@
+/bin/rm -f *.class
+/bin/rm -rf ~/cs143b-project1-submission-vallesja
+/bin/mkdir ~/cs143b-project1-submission-vallesja
+/bin/cp *.java ~/cs143b-project1-submission-vallesja
+/bin/cp README.md ~/cs143b-project1-submission-vallesja
+
+#javac *.java && java ManagerDriver < test/input.txt > output.txt
+#/bin/cp ./output.txt ~/Desktop/cs143b-project1-submission-vallesja
